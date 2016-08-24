3d3844a4bf76439745cfd753ca5d85b8392c8077
ajackson-SE <alanj853@outlook.com>
2016-08-24 10:33:24 +0100

    commit 16

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define alan 123
+
+void func1()
+{
+	printf("Hello world\n");
+}
+
 int main()
 {
 	printf("Hello World. this is a message\n");
 	int g = 88;
 	int xsadf = 5672;
 	int y = 23;// comment 1
+	func1();
 }
